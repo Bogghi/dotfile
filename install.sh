6ea65dbd68b32521c4b457853def5a28c974ae2e
Matteo Simone Borghi <matteosimoneborghi@gmail.com>
2024-08-26 09:57:23 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle install --file=~/.dotfile/Brewfile
