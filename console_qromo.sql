0c9e45f3094d202b85d20b30e9091e4115988a2a
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2024-10-23 11:49:54 +0200

    updated various file

--- console_qromo.sql
+++ console_qromo.sql
@@ -1,9 +1,25 @@
 #### Query for filters in products
 
 select distinct permission from users_businesses;
-select distinct permission from users_businesses;# where business_id = 605;
+select distinct permission from users_businesses;
 select * from orders;
 # questa è la foreign key di users_business
 select * from users;
 select distinct business_id from users_businesses where user_id = 2;
 select * from users_businesses where business_id = 605 and user_id = 10;
+select * from users where email like 'borghi%';
+select * from users_businesses where user_id = 674;
+select order_id, type, date from orders where type = 'cash' order by date desc;
+select * from users;
+select * from orders order by date desc;
+select * from orders where order_id = 31031;
+
+#### Query email
+
+select * from terminals where location_id = 'tml_FveQWgEguGNJpY';
+select distinct default_stripe_location_id from businesses_options;
+select * from businesses_options where default_stripe_location_id = 'tml_FveQWgEguGNJpY';
+select * from payments order by payment_id desc;
+select type from orders order by order_id desc;
+select plan_id, plan_cycle, plan_plugin_pos, plan_plugin_counter, name from businesses;
+select distinct network from transactions;
